63952ee77f24dd1275a636a3688a6a0b470b05c9
GloriouS <68236786+Balick@users.noreply.github.com>
2022-04-22 23:08:19 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-amber-400 text-2xl">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
