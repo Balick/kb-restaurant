db9bf873c5c1c8494c3e0735883e700f476de1b0
Théo Balick <balickmethens@gmail.com>
2024-06-13 23:16:25 +0100

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,91 @@
-import { Main, SideBarLeft } from "./Components/Components";
+import React from "react";
+
+const titles = ["Ecratose", "SolmazoE", "ZozoligE", "Gistagor"];
+const images = [
+  "/images/burger.png",
+  "/images/burger2.png",
+  "/images/burger3.png",
+  "/images/burger5.png",
+];
+const names = [
+  "Ecratose 007",
+  "SolmazoE 008",
+  "ZozoligE 004",
+  "Gistagor 002",
+];
+
+function TitleList() {
+  return (
+    <div className="flex flex-col items-center">
+      {titles.map((title, index) => (
+        <h1
+          key={index}
+          className={`opacity-0 -translate-y-full text-orange-900 absolute left-1/2 -translate-x-1/2 top-1/4 text-[18rem] leading-[1] uppercase title`}
+        >
+          {title}
+        </h1>
+      ))}
+    </div>
+  );
+}
+
+function ImageList() {
+  return (
+    <div>
+      {images.map((src, index) => (
+        <img
+          key={index}
+          src={src}
+          alt="Burger"
+          className={`scale-50 translate-y-full absolute top-[30%] md:top-0 md:pt-12 left-[48%] -translate-x-1/2 flex items-center justify-center w-[800px] object-cover image ${
+            index === 0 ? "w-[650px]" : ""
+          }`}
+        />
+      ))}
+    </div>
+  );
+}
+
+function NameList() {
+  return (
+    <div className="w-full flex justify-center items-center z-10">
+      {names.map((name, index) => (
+        <p
+          key={index}
+          className={`p text-4xl font-light tracking-[1rem] uppercase w-full absolute bottom-10 left-1/2 -translate-x-1/2 text-center name ${
+            index !== 0 ? "opacity-0" : ""
+          }`}
+        >
+          {name}
+        </p>
+      ))}
+    </div>
+  );
+}
+
+function ArrowDirection() {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full flex items-center justify-between py-2 px-20 z-30">
+      <div className="arrow-left cursor-pointer">
+        <img src="/arrow-left.svg" alt="Arrow" className="w-20" />
+      </div>
+      <div className="arrow-right cursor-pointer">
+        <img src="/arrow-right.svg" alt="Arrow" className="w-20" />
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="container w-[100%] relative mx-auto">
-      <SideBarLeft className="w-fit" />
-      <Main />
-    </div>
+    <main className="relative w-full overflow-hidden text-black">
+      <div className="relative w-full h-screen flex items-center justify-center p-4 main">
+        <TitleList />
+        <ImageList />
+        <NameList />
+      </div>
+      <ArrowDirection />
+    </main>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,78 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./style.scss";
+import gsap from "gsap";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const ease = "elastic.out(1,0.85)";
+const duration = 1.5;
+
+function Main() {
+  useEffect(() => {
+    const main = document.querySelector(".main");
+    const arrowLeft = document.querySelector(".arrow-left");
+    const arrowRight = document.querySelector(".arrow-right");
+    const titles = document.querySelectorAll(".title");
+    const names = document.querySelectorAll(".name");
+    const images = document.querySelectorAll(".image");
+
+    let current = 0;
+
+    const update = () => {
+      /*const backgrounds = [
+        "radial-gradient(83.69% 83.69% at 50% 35.84%, #24D381 0%, #00725B 100%)",
+        "radial-gradient(83.69% 83.69% at 50% 35.84%, #E3DC25 0%, #E8C000 67.71%)",
+        "radial-gradient(83.69% 83.69% at 50% 35.84%, #C91920 0%, #6F0012 100%)",
+        "radial-gradient(83.69% 83.69% at 50% 35.84%, #9C00E9 0%, #5F00E0 100%)",
+      ];*/
+
+      //main.style.background = backgrounds[current];
+
+      titles.forEach((title, index) => {
+        gsap.to(title, {
+          duration: duration,
+          ease: ease,
+          opacity: index === current ? 1 : 0,
+          y: index === current ? 0 : "-200%",
+        });
+      });
+
+      names.forEach((name, index) => {
+        gsap.to(name, {
+          duration: duration,
+          ease: ease,
+          opacity: index === current ? 1 : 0,
+        });
+      });
+
+      images.forEach((image, index) => {
+        gsap.to(image, {
+          duration: duration,
+          ease: ease,
+          y: index === current ? 0 : "200%",
+          scale: index === current ? 1 : 0.5,
+        });
+      });
+    };
+
+    arrowLeft.addEventListener("click", () => {
+      current = (current - 1 + titles.length) % titles.length;
+      update();
+    });
+
+    arrowRight.addEventListener("click", () => {
+      current = (current + 1) % titles.length;
+      update();
+    });
+
+    update();
+  }, []);
+
+  return <App />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Main />
+  </React.StrictMode>
+);
